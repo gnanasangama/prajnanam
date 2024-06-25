ff02d13296fde080ff8638fefe1b20888714c6ad
akshay_gj <akshay@glynk.com>
2024-06-25 22:22:45 +0530

    minor fix

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('pwa-cache-v1').then((cache) => {
             return cache.addAll([
-                '/prajananam/',
-                '/prajananam/index.html',
-                '/prajananam/style.css',
+                '/prajnanam/',
+                '/prajnanam/index.html',
+                '/prajnanam/style.css',
                 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
             ]);
         })
