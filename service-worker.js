5b54b19eb237b9d007311cd028eed96b3d1beffc
akshay_gj <akshay@glynk.com>
2024-06-25 23:36:38 +0530

    added ashada 2024 [WIP]

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,8 @@ self.addEventListener('install', (event) => {
                 '/',
                 '/index.html',
                 '/style.css',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
             ]);
         })
     );
