4769a622e763d64517df4315cb0436d231cf8491
akshaygj <agjworkspace@gmail.com>
2025-06-28 21:48:31 +0530

    updated next month content

--- assets/js/render.js
+++ assets/js/render.js
@@ -56,7 +56,7 @@ function renderShlokas() {
             ${content.shlokas.map((week, index) => `
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="heading${index}">
-                        <button class="accordion-button ${index === 3 ? '' : 'collapsed'}" type="button" 
+                        <button class="accordion-button ${index === 0 ? '' : 'collapsed'}" type="button" 
                             data-bs-toggle="collapse" data-bs-target="#collapse${index}" 
                             aria-expanded="${index === 0 ? 'true' : 'false'}" 
                             aria-controls="collapse${index}">
@@ -64,7 +64,7 @@ function renderShlokas() {
                         </button>
                     </h2>
                     <div id="collapse${index}" 
-                        class="accordion-collapse collapse ${index === 3 ? 'show' : ''}" 
+                        class="accordion-collapse collapse ${index === 0 ? 'show' : ''}" 
                         aria-labelledby="heading${index}" 
                         data-bs-parent="#shlokaAccordion">
                         <div class="accordion-body p-0">
